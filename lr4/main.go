673736fb5c30e3949da19fcaa3083be52f3762cf
Chopper <doc.chopperx@gmail.com>
2025-05-05 02:45:50 +0300

    lr4 is almost done, need refactoring

--- lr4/main.go
+++ lr4/main.go
@@ -17,41 +17,50 @@ import (
 // "github.com/awalterschulze/gographviz"
 
 type node struct {
-    key             int
+    key             string
     susessors       []*node
     // strategyCount   int 
     player          byte
-    costs           []int
-
+    parent          *node
+    costs           [][]int
+    color           byte
 }
 
 type Tree struct {
     root *node
 }
 
+var leaves []*node = []*node{}
 const depth int = 5
 
-func add(p byte, d int, n *node, g *gographviz.Graph) *node {
+func add(p byte, d int, n *node, par *node, g *gographviz.Graph) *node {
     if d == depth {
-        c := make([]int, 2)
-        for i := range c {
-            c[i] = int(math.Pow(-1, float64(rand.Intn(2)))) * rand.Intn(21)
-        }
+        // c := make([]int, 2)
+        // for i := range c {
+        //     c[i] = int(math.Pow(-1, float64(rand.Intn(2)))) * rand.Intn(21)
+        // }
+        c := [][]int{}
+        c = append(c, []int{int(math.Pow(-1, float64(rand.Intn(2)))) * rand.Intn(21), int(math.Pow(-1, float64(rand.Intn(2)))) * rand.Intn(21)})
 
 
-        r := rand.Intn(1000000)
-        label := fmt.Sprintf("\"%s\\n%d\\n(%d, %d)\"", string(p), r, c[0], c[1])
+        r := strconv.Itoa(rand.Intn(1000000))
+        label := fmt.Sprintf("\"%s\\n%s\\n(%d, %d)\"", string(p), r, c[0][0], c[0][1])
         attrs := map[string]string {
             "label": label,
         }
-        g.AddNode("G", strconv.Itoa(r), attrs)
-        return &node {
+        g.AddNode("G", r, attrs)
+        // g.AddNode("G", r, nil)
+        leaf := &node {
             key:            r,
             susessors:      nil,
             // strategyCount:  0,
             player:         p,
+            parent:         par,
             costs:          c,
+            color:          'w',
         }
+        leaves = append(leaves, leaf)
+        return leaf
     }
 
 
@@ -66,26 +75,29 @@ func add(p byte, d int, n *node, g *gographviz.Graph) *node {
             size = 3
         }
 
-        r := rand.Intn(1000000)
-        label := fmt.Sprintf("\"%s\\n%d\"", string(p), r)
+        r := strconv.Itoa(rand.Intn(1000000))
+        label := fmt.Sprintf("\"%s\\n%s\"", string(p), r)
         attrs := map[string]string {
             "label": label,
         }
-        g.AddNode("G", strconv.Itoa(r), attrs)
+        g.AddNode("G", r, attrs)
+        // g.AddNode("G", r, nil)
         
         n = &node{
             key:            r,
             susessors:      make([]*node, size),
             // strategyCount:  3,
             player:         p,
+            parent:         par,
+            color:          'w',
             costs:          nil,
         }
 
         d++
 
         for i := range n.susessors {
-            n.susessors[i] = add(pl, d, n.susessors[i], g)
-            g.AddEdge(strconv.Itoa(r), strconv.Itoa(n.susessors[i].key), true, nil)
+            n.susessors[i] = add(pl, d, n.susessors[i], n, g)
+            g.AddEdge(r, n.susessors[i].key, true, nil)
         }
     }
 
@@ -109,9 +121,114 @@ func GenerateTree(g *gographviz.Graph) *Tree {
 
     d := 0
 
-    t.root = add(players[rand.Intn(len(players))], d, t.root, g)
+    t.root = add(players[rand.Intn(len(players))], d, t.root, nil, g)
 
     return t
+}
+
+func ReverseInductionMethod(g *gographviz.Graph) {
+
+    for len(leaves) > 0 {
+        u := leaves[0]
+        leaves = leaves[1:]
+        
+        if u != nil && u.parent != nil {
+            if u.parent.color == 'w' {
+                // maxc := []int{}
+                maxc := u.costs[0]
+                var idx int
+                if u.player == 'A' {
+                    idx = 0
+                } else {
+                    idx = 1
+                }
+                // r := u
+                tmp := u
+                for _, v := range u.parent.susessors {
+                    for _, vv := range v.costs {
+                        if vv[idx] > maxc[idx] {
+                            maxc = vv
+                            tmp = v
+                        }
+                    }
+
+                    // if v.costs[idx] > maxc[idx] {
+                    //     maxc = v.costs
+                    //     // r = v
+                    // }
+                }
+                u.parent.costs = append(u.parent.costs, maxc)
+                tmp.color = 'v'
+                for _, v := range u.parent.susessors {
+                    for _, vv := range v.costs {
+                        if vv[idx] == maxc[idx] && v.color != 'v'{
+                            u.parent.costs = append(u.parent.costs, vv)
+                            v.color = 'v'
+                        }
+                    }
+                }
+
+                n := g.Nodes.Lookup[u.parent.key]
+                
+                var strc string
+                for _, v := range u.parent.costs {
+                    strc = strc + fmt.Sprintf("(%d, %d)\\n", v[0], v[1])
+                }
+
+                // n.Attrs["label"] = fmt.Sprintf("\"%s\\n%s\\n(%d, %d)\"", string(u.parent.player), u.parent.key, u.parent.costs[0][0], u.parent.costs[0][1])
+                n.Attrs["label"] = fmt.Sprintf("\"%s\\n%s\\n%s\"", string(u.parent.player), u.parent.key, strc)
+
+                // e := g.Edges.SrcToDsts[u.parent.key][r.key][0]
+                // e.Attrs["color"] = "red"
+                // e.Attrs["penwidth"] = "2"
+                
+                leaves = append(leaves, u.parent)
+                u.parent.color = 'b'
+            }
+        }
+        // u.parent.costs 
+
+    }
+
+}
+
+
+func PreOrderG(t *node, g *gographviz.Graph) {
+
+    // c := t.root.costs
+    for _, c := range t.costs {
+        var traverse func(*node)
+
+        check := func(a []int) bool {
+            for i := range a {
+                if a[i] != c[i] {
+                    return false
+                }
+            }
+            return true
+        }
+
+        traverse = func(n *node) {
+            if n == nil {
+                return
+            }
+            for _, v := range n.susessors {
+                for _, vv := range v.costs {
+                    if check(vv) {
+                        PreOrderG(v, g)
+                        e := g.Edges.SrcToDsts[n.key][v.key][0]
+                        e.Attrs["color"] = "red"
+                        e.Attrs["penwidth"] = "2"
+                    }
+                }
+                traverse(v)
+            }
+        }
+
+        traverse(t)
+
+    }
+
 }
 
 func main() {
@@ -124,14 +241,22 @@ func main() {
     }
 
     t := GenerateTree(graph)
+    ReverseInductionMethod(graph)
+    PreOrderG(t.root, graph)
     fmt.Println(t)
 
     // TEST
 
-
     // graph.AddNode("G", "a", nil)
     // graph.AddNode("G", "b", nil)
-    // graph.AddEdge("a", "b", true, nil)
+    // graph.AddEdge("a", "b", true, map[string]string {
+    //     "color":    "red",
+    //     "penwidth": "5",
+    // })
+    // e := graph.Edges.SrcToDsts["a"]["b"][0]
+    // e.Attrs["color"] = "green"
+    // e.Attrs["penwidth"] = "3"
+
     output := graph.String()
     fmt.Println(output)
 
@@ -143,6 +268,9 @@ func main() {
         log.Fatal("Error converting DOT to PNG: ", err)
     }
 
+    fmt.Println(leaves)
+    fmt.Println(len(leaves))
+
     // img, err := imaging.Open("graph.png") 
     // if err != nil {
     //     log.Fatal(err)
@@ -152,5 +280,4 @@ func main() {
     // err = imaging.Save(resize, "graph_resized.png")
     // if err != nil {
     //     log.Fatal(err)
-    // }
-}
+    }
