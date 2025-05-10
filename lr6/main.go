8ceca6e6504e2bf3ff74a34b4d200201413b3a0d
Chopper <doc.chopperx@gmail.com>
2025-05-10 14:30:09 +0300

    refactoring lr6

--- lr6/main.go
+++ lr6/main.go
@@ -10,6 +10,7 @@ import (
 
 
 var N = 10
+var EPSILON = 1e-6
 
 
 func PrintMatrix(m [][]float64) {
@@ -21,6 +22,8 @@ func PrintMatrix(m [][]float64) {
     }
     fmt.Println()
 }
+
+// Генерация случайной стохастической матрицы доверия
 
 func GenerationStochasticMatrix(n int) [][]float64 {
 
@@ -45,6 +48,8 @@ func GenerationStochasticMatrix(n int) [][]float64 {
     return r
 } 
 
+// Нахождение максимального и минимального значения в срезе
+
 func MaxSlice(a []float64) float64 {
     max := a[0]
     for _, v := range a {
@@ -67,48 +72,43 @@ func MinSlice(a []float64) float64 {
     return min
 } 
 
+// Функция вычисления итогового мнения агентов, до схождения матрицы с заданной точностью epsilon
+
+func Calc(epsilon float64, A [][]float64, x0 []float64) (int, []float64) {
+    t := 1
+    e := MaxSlice(x0) - MinSlice(x0)
+    xt := make([]float64, N)
+    copy(xt, x0)
+
+    for e > epsilon {
+        for i := 0; i < len(A); i++ {
+            x := 0.0
+            for j := 0; j < len(A); j++ {
+                x += A[i][j] * xt[j]
+                // s := 0.0
+                // for k := 0; k < len(A); k++ {
+                    // s += A[i][k] * B[k][j]
+                // }
+                // B[i][j] = s
+            }
+            xt[i] = x
+        }
+        e = MaxSlice(xt) - MinSlice(xt)
+        t += 1
+    }
+
+    return t, xt
+}
+
+
 func main() {
 
     rand.New(rand.NewSource(time.Now().UnixNano()))
 
-    /*
-
-
-    Test := [][]float64 {
-        {0.111, 0.178, 0.003, 0.074, 0.152, 0.131, 0.145, 0.101, 0.104},
-        {0.045, 0.125, 0.076, 0.086, 0.112, 0.11, 0.207, 0.125, 0.112},
-        {0.006, 0.214, 0.095, 0.038, 0.038, 0.142, 0.162, 0.275, 0.032},
-        {0.168, 0.164, 0.094, 0.094, 0.007, 0.105, 0.15, 0.075, 0.143},
-        {0.046, 0.258, 0.093, 0.113, 0.07, 0.006, 0.18, 0.113, 0.122},
-        {0.198, 0.175, 0.07, 0.039, 0.187, 0.022, 0.064, 0.12, 0.125},
-        {0.196, 0.077, 0.134, 0.151, 0.047, 0.223, 0.139, 0.017, 0.017},
-        {0.049, 0.201, 0.009, 0.099, 0.077, 0.296, 0.056, 0.006, 0.207},
-        {0.068, 0.036, 0.155, 0.138, 0.148, 0.058, 0.226, 0.019, 0.15},
-    }
-    PrintMatrix(Test)
-
-    TestXt := [9]float64{15, 14, 19, 8, 12, 1, 16, 11, 17}
-    for t := 0; t < 11; t++ {
-        for i := 0; i < len(TestXt); i++ {
-            x := 0.0
-            for j := 0; j < len(TestXt); j++ {
-                x += Test[i][j] * TestXt[j] 
-            }
-            TestXt[i] = x
-        }
-    }
-
-    fmt.Println(TestXt)
-
-    */
-
-    // TestX0 := []int {15, 14, 19, 8, 12, 1, 16, 11, 17}
-
-
     A := GenerationStochasticMatrix(N)
     PrintMatrix(A)
 
-    // Выбор рандомного подмножества агентов для игрока A
+    // Выбор случайного подмножества агентов для игрока A
 
     agents := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
@@ -160,46 +160,67 @@ func main() {
     fmt.Printf("agents B: %v\n", agentsB)
     fmt.Println()
 
+    printXt := func(t int, xt []float64) {
+        fmt.Printf("x(%d) = [ ", t)
+        for _, v := range xt {
+            fmt.Printf("%.3f  ", v)
+        }
+        fmt.Println("]")
+        fmt.Println()
+    }
+
     // Герератор вектора начальных мнений агентов
 
     x0 := make([]float64, N)
     for i := range x0 {
         x0[i] = float64(rand.Intn(50))
     }
+    printXt(0, x0)
 
-    fmt.Printf("x(0) = [  ")
-    for _, v := range x0 {
-        fmt.Printf("%.3f  ", v)
-    }
-    fmt.Println("]")
 
-    epsilon := 1e-6
-
-    xt := make([]float64, N)
-    copy(xt, x0)
+    // xt := make([]float64, N)
+    // copy(xt, x0)
 
     // Вычисление результирующего мнения агентов
 
-    t := 1
-    e := MaxSlice(x0) - MinSlice(x0)
-    for e > epsilon {
-        for i := 0; i < len(A); i++ {
-            x := 0.0
-            for j := 0; j < len(A); j++ {
-                x += A[i][j] * xt[j]
-            }
-            xt[i] = x
-        }
-        e = MaxSlice(xt) - MinSlice(xt)
-        t += 1
-    }
+    // t := 1
+    // e := MaxSlice(x0) - MinSlice(x0)
+    // B := make([][]float64, len(A))
+    // for i := range B {
+    //     B[i] = make([]float64, len(A))
+    // }
+    //
+    // for i := range A {
+    //     for j := range A[i] {
+    //         B[i][j] = A[i][j]
+    //     }
+    // }
+    //
+    // for e > epsilon {
+    //     for i := 0; i < len(A); i++ {
+    //         x := 0.0
+    //         for j := 0; j < len(A); j++ {
+    //             x += A[i][j] * xt[j]
+    //             s := 0.0
+    //             for k := 0; k < len(A); k++ {
+    //                 s += A[i][k] * B[k][j]
+    //             }
+    //             B[i][j] = s
+    //         }
+    //         xt[i] = x
+    //     }
+    //     e = MaxSlice(xt) - MinSlice(xt)
+    //     t += 1
+    // }
 
-    fmt.Printf("x(%d) = [  ", t)
-    for _, v := range xt {
-        fmt.Printf("%.3f  ", v)
-    }
-    fmt.Println("]")
-    fmt.Println()
+    // fmt.Println()
+    // PrintMatrix(B)
+
+
+    t, xt := Calc(EPSILON, A, x0)
+    printXt(t, xt)
+
+    // генерация начального мнения u, v для агентов первого (А) и второго (В) игроков соответственно
 
     u := rand.Intn(100)
     v := rand.Intn(100) - 100
@@ -215,33 +236,10 @@ func main() {
     for _, x := range agentsB {
         x0[x - 1] = float64(v)
     }
+    printXt(0, x0)
 
-    fmt.Printf("x(0) = [  ")
-    for _, v := range x0 {
-        fmt.Printf("%.3f  ", v)
-    }
-    fmt.Println("]")
-
-    t = 1
-    e = MaxSlice(x0) - MinSlice(x0)
-    copy(xt, x0)
-    for e > epsilon {
-        for i := 0; i < len(A); i++ {
-            x := 0.0
-            for j := 0; j < len(A); j++ {
-                x += A[i][j] * xt[j]
-            }
-            xt[i] = x
-        }
-        e = MaxSlice(xt) - MinSlice(xt)
-        t += 1
-    }
-
-    fmt.Printf("x(%d) = [  ", t)
-    for _, v := range xt {
-        fmt.Printf("%.3f  ", v)
-    }
-    fmt.Println("]")
+    t, xt = Calc(EPSILON, A, x0)
+    printXt(t, xt)
 
     if math.Abs(float64(u) - xt[0]) > math.Abs(float64(v) - xt[0]) {
         fmt.Println("Победил второй игрок")
